823dfaff5276fb67832f8f0c107ac9e662d07b8f
Takahiro Masui <takahiro.masui@luxiar.com>
2020-09-20 21:39:05 +0900

    退会ユーザー

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :events do
     resources :tickets
   end
+  resource :retirements
   root 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
