cc7e89bb19637a15279396eb7adb0f4e217b44f4
Takahiro Masui <takahiro.masui@luxiar.com>
2020-09-20 17:50:57 +0900

    イベントの参加をキャンセル

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -13,6 +13,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @ticket = current_user&.tickets&.find_by(event: @event)
     @tickets = @event.tickets.includes(:user).order(:created_at)
   end
 
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -12,4 +12,11 @@ class TicketsController < ApplicationController
 
     redirect_to event, notice: 'このイベントに参加表明しました' if @ticket.save
   end
+
+  def destroy
+    # tickets = current_user.tickets.find(params[:id])
+    tickets = current_user.tickets.find_by!(event_id: params[:event_id])
+    tickets.destroy!
+    redirect_to event_path(params[:event_id]), notice: 'このイベントをキャンセルしました'
+  end
 end
